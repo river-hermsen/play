2059c6ad7c5a93b31f54c0f58e125b0e6d8e8860
River Hermsen <riverhermsen@hotmail.nl>
2019-06-23 17:13:20 +0200

    Added random genres to browse page.

--- client/src/views/Browse.vue
+++ client/src/views/Browse.vue
@@ -38,9 +38,43 @@
           </el-col>
         </el-row>
       </div>
-      <!-- <div class="randomgenres">
-        <div v-for="genre in podcasts.genres" :key="genre.id">{{genre.name}}</div>
-      </div>-->
+      <el-divider></el-divider>
+
+      <div class="randomgenres">
+        <div>
+          <h2>Best of {{podcasts.randomGenre.genre.name}}</h2>
+          <el-row>
+            <el-col
+              v-for="podcast in podcasts.randomGenre.podcasts"
+              :key="podcast.id"
+              :md="8"
+              :lg="6"
+              :xl="6"
+            >
+              <el-card class="cardPodcast">
+                <img :src="podcast.thumbnail" class="imagePodcast">
+                <div class="infoBoxPodcast">
+                  <div class="titlePodcast">
+                    <b :title="podcast.title">{{podcast.title}}</b>
+                  </div>
+                  <div>
+                    <span>
+                      <span
+                        class="genrePodcast"
+                      >{{$store.getters.getGenreTitleById(podcast.genre_ids[0]).name}}</span>
+                      <!-- <span
+                      v-for="genre in podcast.genre_ids.slice(0, 1)"
+                      :key="genre"
+                      >{{$store.getters.getGenreTitleById(genre).name}}</span>-->
+                    </span>
+                    <span class="languagePodcast">{{podcast.language}}</span>
+                  </div>
+                </div>
+              </el-card>
+            </el-col>
+          </el-row>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -74,6 +108,7 @@
   overflow: hidden;
   text-overflow: ellipsis;
   font-size: 1.2rem;
+  height: 1.3rem;
 }
 
 .languagePodcast {
@@ -100,12 +135,17 @@ export default {
       },
       podcasts: {
         genres: "",
-        mostPopular: []
+        mostPopular: [],
+        randomGenre: {
+          genre: {},
+          podcasts: {}
+        }
       }
     };
   },
   created() {
     console.log(this.$store.getters.getRandomGenre);
+
     // console.log(this.$store.getters.getGenreTitleById(67));
     // Get user info
     this.$store.dispatch("getUserInfo").then(userInfo => {
@@ -153,6 +193,26 @@ export default {
           console.log(err);
         });
     }
+
+    // Get a random genre and associated podcasts
+    this.podcasts.randomGenre.genre = this.$store.getters.getRandomGenre;
+    axios
+      .get(
+        `https://listen-api.listennotes.com/api/v2/best_podcasts?genre_id=${
+          this.podcasts.randomGenre.genre.id
+        }`,
+        {
+          headers: { "X-ListenAPI-Key": "2e2c4f39b7b44659b73cb3b31f95236e" }
+        }
+      )
+      .then(res => {
+        console.log(res.data);
+        this.podcasts.randomGenre.podcasts = res.data.podcasts.slice(0, 8);
+        console.log(this.podcasts.randomGenre.podcasts.slice(0, 8));
+      })
+      .catch(err => {
+        console.log(err);
+      });
   },
   methods: {
     getGenreTitleById(id) {
